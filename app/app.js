a22ff0efbb33e8c115ec3a471219fd2dcc5d0574
Michael Lamb <503lamb@gmail.com>
2018-03-26 13:19:33 -0600

    fixes

--- app/app.js
+++ app/app.js
@@ -4,41 +4,17 @@ var template = '';
 function showDeets(n) {
 
     if (n == 1) {
-        if (template.length > 0) {
-            template = ''
-        } else {
-            template = `<p>Able to speak, read and write in spanish.</p>`
-        }
+        template = `<p>Able to speak, read and write in spanish.</p>`
     } else if (n == 2) {
-        if (template.length > 0) {
-            template = ''
-        } else {
-            template = `<p>Played Pac-12 College Football for 5 years.</p>`
-        }
+        template = `<p>Played Pac-12 College Football for 5 years.</p>`
     } else if (n == 3) {
-        if (template.length > 0) {
-            template = ''
-        } else {
-            template = `<p>I tap dance. Started in middle school and it has stuck with me ever since.</p>`
-        }
+        template = `<p>I tap dance. Started in middle school and it has stuck with me ever since.</p>`
     } else if (n == 4) {
-        if (template.length > 0) {
-            template = ''
-        } else {
-            template = `<p>I love traveling and experiencing new cultures.</p>`
-        }
+        template = `<p>I love traveling and experiencing new cultures.</p>`
     } else if (n == 5) {
-        if (template.length > 0) {
-            template = ''
-        } else {
-            template = `<p>Volunteer at Habitat for Humanity, community centers, and schools.</p>`
-        }
+        template = `<p>Volunteer at Habitat for Humanity, community centers, and schools.</p>`
     } else {
-        if (template.length > 0) {
-            template = ''
-        } else {
-            template = `<p>Currently my favorite song is Rocket Man by Elton John</p>`
-        }
+        template = `<p>Currently my favorite song is Rocket Man by Elton John</p>`
     }
 
     injectorElem.innerHTML = template
